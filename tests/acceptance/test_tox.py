16fd8c95a82f75d8c75ed832209ca540c8a22119
Stephen Rosen <sirosen@globus.org>
2025-10-04 19:15:28 -0500

    Set pytest no:legacypath and convert to tmp_path

--- tests/acceptance/test_tox.py
+++ tests/acceptance/test_tox.py
@@ -1,10 +1,11 @@
+import contextlib
 from textwrap import dedent as d
 
 
-def test_read_min_version(tmpdir, run_line):
-    toxini = tmpdir.join("tox.ini")
+def test_read_min_version(tmp_path, run_line):
+    toxini = tmp_path / "tox.ini"
 
-    toxini.write(
+    toxini.write_text(
         d(
             """\
             [tox]
@@ -16,14 +17,14 @@ def test_read_min_version(tmpdir, run_line):
         )
     )
 
-    with tmpdir.as_cwd():
+    with contextlib.chdir(tmp_path):
         run_line("mddj read tox min-version", search_stdout="3.5")
 
 
-def test_read_version_list(tmpdir, run_line):
-    toxini = tmpdir.join("tox.ini")
+def test_read_version_list(tmp_path, run_line):
+    toxini = tmp_path / "tox.ini"
 
-    toxini.write(
+    toxini.write_text(
         d(
             """\
             [tox]
@@ -35,16 +36,16 @@ def test_read_version_list(tmpdir, run_line):
         )
     )
 
-    with tmpdir.as_cwd():
+    with contextlib.chdir(tmp_path):
         result = run_line("mddj read tox list-versions")
 
     assert result.stdout == "3.5\n3.6\n3.7\n3.8\n3.9\n3.10\n"
 
 
-def test_read_version_list_with_repeats_and_factors(tmpdir, run_line):
-    toxini = tmpdir.join("tox.ini")
+def test_read_version_list_with_repeats_and_factors(tmp_path, run_line):
+    toxini = tmp_path / "tox.ini"
 
-    toxini.write(
+    toxini.write_text(
         d(
             """\
             [tox]
@@ -58,7 +59,7 @@ def test_read_version_list_with_repeats_and_factors(tmpdir, run_line):
         )
     )
 
-    with tmpdir.as_cwd():
+    with contextlib.chdir(tmp_path):
         result = run_line("mddj read tox list-versions")
 
     assert result.stdout == "3.5\n3.6\n3.7\n3.8\n3.9\n3.10\n"
