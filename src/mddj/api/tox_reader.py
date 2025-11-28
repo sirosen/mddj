985210a6cdb29ead20459846a1d7c04cdc598e28
Stephen Rosen <sirosen@globus.org>
2025-11-28 01:35:19 -0600

    Add tests to target coverage gaps
    Also, slightly tweak the tox logic to be more similar to `main`.

--- src/mddj/api/tox_reader.py
+++ src/mddj/api/tox_reader.py
@@ -5,7 +5,6 @@ import re
 import shutil
 import subprocess
 import sys
-import typing as t
 
 
 class ToxReaderError(RuntimeError):
@@ -42,29 +41,33 @@ class ToxReader:
             return (sys.executable, "-m", "tox")
 
     @functools.cached_property
-    def _tox_version(self) -> t.Literal[3, 4]:
-        """
-        Check that the 'tox' command is a supported version and return it.
-
-        Any unexpected outputs will raise an error, which allows for a cleaner early
-        abort.
-        """
+    def _tox_version(self) -> str:
         tox_version_proc = subprocess.run(
             [*self._tox_cmd, "--version"], text=True, capture_output=True
         )
         if tox_version_proc.returncode != 0:
             raise ToxReaderError("Cannot fetch tox data. 'tox --version' failed.")
 
-        full_tox_version = tox_version_proc.stdout.strip()
-        if full_tox_version.startswith("3."):
-            return 3
-        elif full_tox_version.startswith("4."):
-            return 4
+        return tox_version_proc.stdout.strip().partition(" ")[0]
+
+    def _check_tox_version(self) -> str:
+        """
+        Check that the 'tox' command is a supported version and return it.
+
+        Any unexpected outputs will raise an error, which allows for a cleaner early
+        abort.
+        """
+        from packaging.version import Version
+
+        version = Version(self._tox_version)
+        if version.major in (3, 4):
+            return self._tox_version
         else:
             raise ToxReaderError("'tox --version' was not a recognized version.")
 
     @functools.cached_property
     def _tox_listenvs(self) -> tuple[str, ...]:
+        self._check_tox_version()
         output = subprocess.check_output([*self._tox_cmd, "--listenvs"], text=True)
         return tuple(line for line in output.splitlines() if line)
 
