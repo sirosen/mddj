803abc29f01723b5531a43a586d09eaaef29e2d3
Stephen Rosen <sirosen@globus.org>
2025-12-15 16:13:23 -0600

    Move acceptance tests of 'read python-requires'

--- tests/acceptance/test_pyprojecttoml.py
+++ tests/acceptance/test_pyprojecttoml.py
@@ -31,39 +31,6 @@ def test_read_simple_project_attrs(chdir, tmp_path, run_line, keyname):
         run_line(cmd, search_stdout=rf"^{re.escape(expect_value)}$")
 
 
-@pytest.mark.parametrize("lower_bound", (True, False))
-def test_read_python_requires(chdir, tmp_path, run_line, lower_bound):
-    pyproject = tmp_path / "pyproject.toml"
-
-    pyproject.write_text(
-        d(
-            """\
-            [build-system]
-            requires = ["setuptools"]
-            build-backend = "setuptools.build_meta"
-
-            [project]
-            name = "foopkg"
-            version = "1.0.0"
-            authors = [
-              { name = "Foo", email = "foo@example.org" },
-            ]
-            requires-python = ">=3.11"
-            """
-        ),
-        encoding="utf-8",
-    )
-    (tmp_path / "foopkg.py").touch()
-
-    with chdir(tmp_path):
-        cmd = ["mddj", "read", "requires-python"]
-        if lower_bound:
-            cmd.append("--lower-bound")
-
-        expect_result = r"^3\.11$" if lower_bound else r"^>=3\.11$"
-        run_line(cmd, search_stdout=expect_result)
-
-
 def test_read_version_from_pyproject(chdir, tmp_path, run_line):
     pyproject = tmp_path / "pyproject.toml"
 
--- tests/acceptance/test_setupcfg.py
+++ tests/acceptance/test_setupcfg.py
@@ -1,37 +1,6 @@
 from textwrap import dedent as d
 
 
-def test_read_python_requires(chdir, tmp_path, run_line, capfd):
-    setupcfg = tmp_path / "setup.cfg"
-
-    setupcfg.write_text(
-        d(
-            """\
-            [metadata]
-            name = foopkg
-            version = 1.0.0
-
-            author = Foo
-            author_email = foo@example.org
-
-            [options]
-            python_requires = >=3.10
-            """
-        ),
-        encoding="utf-8",
-    )
-    (tmp_path / "setup.py").write_text(
-        "from setuptools import setup; setup()\n", encoding="utf-8"
-    )
-    (tmp_path / "foopkg.py").touch()
-
-    with chdir(tmp_path):
-        result = run_line("mddj read requires-python", search_stdout=r"^>=3\.10$")
-        assert len(result.stdout.splitlines()) == 1
-    captured = capfd.readouterr()
-    assert captured.out == ""
-
-
 def test_read_python_requires_with_full_build_output_shows_all_data(
     chdir, tmp_path, run_line, capfd
 ):
