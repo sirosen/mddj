368a202baa76539ee3e0f629bd2773caf40f0b70
Stephen Rosen <sirosen@globus.org>
2025-12-19 11:13:41 -0600

    Update extra parsing to produce originals as well
    Do all of the parsing work on optional dependencies at once. The regex
    matches which "find matching requirements" can also be used to do the
    removal of "the extra parts". This makes the handling more uniform for
    any usages which will access both parts of the metadata.
    
    The original extras are now exposed, with the "cleaned" ones not yet
    provided in the API.

--- src/mddj/_internal/_wheel_metadata.py
+++ src/mddj/_internal/_wheel_metadata.py
@@ -21,6 +21,9 @@ class WheelDependencyData:
     #   Provides-Extra
     #   Requires-Dist
     extras: types.MappingProxyType[str, tuple[str, ...]]
+    # a variant of the parsed extras, as above, but with the `extra == ...` markers
+    # removed where possible
+    cleaned_extras: types.MappingProxyType[str, tuple[str, ...]]
     # the non-extra dependencies, as indicated by
     #   Requires-Dist
     # after filtering Provides-Extra
@@ -52,78 +55,70 @@ def load_wheel_dependency_data(
     requires_dist = metadata.get_all("Requires-Dist", [])
 
     extras_map: dict[str, tuple[str, ...]] = {}
+    cleaned_extras_map: dict[str, tuple[str, ...]] = {}
     for extra_name in provides_extra:
-        extras_map[extra_name] = tuple(
-            _read_extra_from_dists(extra_name, requires_dist)
-        )
+        original_dists: list[str] = []
+        cleaned_dists: list[str] = []
+        for original, cleaned in _read_extra_from_dists(extra_name, requires_dist):
+            original_dists.append(original)
+            cleaned_dists.append(cleaned)
+        extras_map[extra_name] = tuple(original_dists)
+        cleaned_extras_map[extra_name] = tuple(cleaned_dists)
 
     extra_deps = [dep for extra_deps in extras_map.values() for dep in extra_deps]
     non_extra_deps = tuple(dep for dep in requires_dist if dep not in extra_deps)
 
-    for k, v in extras_map.items():
-        extras_map[k] = tuple(_strip_extra_from_markers(dist, k) for dist in v)
-
-    return WheelDependencyData(types.MappingProxyType(extras_map), non_extra_deps)
+    return WheelDependencyData(
+        types.MappingProxyType(extras_map),
+        types.MappingProxyType(cleaned_extras_map),
+        non_extra_deps,
+    )
 
 
 def _read_extra_from_dists(
     extra_name: str, requires_dist: t.Iterable[str]
-) -> t.Iterator[str]:
-    marker_patterns = tuple(
-        re.compile(pat)
-        for quote in ('"', "'")
-        for pat in (
-            rf"^\s*extra\s*==\s*{quote}{extra_name}{quote}\s*$",
-            rf"^\s*extra\s*==\s*{quote}{extra_name}{quote}\s+and\s",
-            rf"\sand\s+extra\s*==\s*{quote}{extra_name}{quote}\s*$",
-        )
-    )
+) -> t.Iterator[tuple[str, str]]:
+    """
+    Given an extra name, find all of the matching dependency dist strings.
+
+    Return them as pairs of (original_value, interpreted_value) where
+    the interpreted value is an attempt to strip off the extra name.
+    """
+    subpattern = _extra_pattern(extra_name)
+    exact_pattern = re.compile(rf"^\s*{subpattern}\s*$")
+    trailing_pattern = re.compile(rf"^\s*(.*)\s+and\s+{subpattern}\s*$")
+    leading_pattern = re.compile(rf"^\s*{subpattern}\s+and\s+(.*)\s*$")
+
     for dist_string in requires_dist:
-        # substring matching to reject most things quickly
+        # substring matching to reject most things very quickly, without regex matching
         if "extra" not in dist_string or extra_name not in dist_string:
             continue
-        markers = str(Requirement(dist_string).marker)
-        for pat in marker_patterns:
-            if pat.search(markers):
-                yield dist_string
-                break
+        req = Requirement(dist_string)
+        marker_string = str(req.marker)
 
+        # if an exact match works, strip markers entirely
+        if exact_pattern.search(marker_string):
+            print(f"{exact_pattern=} matched {marker_string=}")
+            req.marker = None
+            yield (dist_string, str(req))
+            continue
 
-def _strip_extra_from_markers(dist_string: str, extra_name: str) -> str:
-    """Attempt to remove an `extra == '...'` from a dist string's markers."""
-    req = Requirement(dist_string)
-    exact_patterns = tuple(
-        re.compile(rf"^\s*extra\s*==\s*{quote}{extra_name}{quote}\s*$")
-        for quote in ('"', "'")
-    )
-    if any(pat.search(str(req.marker).strip()) for pat in exact_patterns):
-        req.marker = None
-        return str(req)
-
-    ending_patterns = tuple(
-        re.compile(rf"^\s*(.*)\s*and\s+extra\s*==\s*{quote}{extra_name}{quote}\s*$")
-        for quote in ('"', "'")
-    )
-    starting_patterns = tuple(
-        re.compile(rf"^\s*extra\s*==\s*{quote}{extra_name}{quote}\s+and\s+(.*)\s*$")
-        for quote in ('"', "'")
-    )
-    marker_str = str(req.marker)
-    new_marker: str | None = None
-    for pat in ending_patterns:
-        if re_match := pat.search(marker_str):
+        # if the leading or trailing pattern matches, the modified marker is the
+        # matched group, with parens stripped if possible
+        if (re_match := leading_pattern.search(marker_string)) or (
+            re_match := trailing_pattern.search(marker_string)
+        ):
             new_marker = re_match.group(1)
-            break
-    else:
-        for pat in starting_patterns:
-            if re_match := pat.search(marker_str):
-                new_marker = re_match.group(1)
-                break
+            if new_marker is None:
+                print(f"{marker_string=} {leading_pattern=} {trailing_pattern=}")
+            # remove parens, and potentially extraneous whitespace within
+            if new_marker.startswith("(") and new_marker.endswith(")"):
+                new_marker = new_marker[1:-1].strip()
+            req.marker = Marker(new_marker)
+            yield (dist_string, str(req))
 
-    if new_marker is None:
-        return dist_string
 
-    if new_marker.startswith("(") and new_marker.endswith(")"):
-        new_marker = new_marker[1:-1]
-    req.marker = Marker(new_marker)
-    return str(req)
+def _extra_pattern(extra_name: str) -> str:
+    escaped_extra_name = re.escape(extra_name)
+    quoted_extra_name = f"(?:(?:'{escaped_extra_name}')|(?:\"{escaped_extra_name}\"))"
+    return r"extra\s*==\s*" + quoted_extra_name
