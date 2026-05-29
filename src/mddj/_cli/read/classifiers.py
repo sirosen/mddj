adbfecb9e603fed24d3122dcc893a931ccd77fa3
Stephen Rosen <sirosen@globus.org>
2026-05-28 22:20:57 -0500

    Add flag to extract versions from classifiers

--- src/mddj/_cli/read/classifiers.py
+++ src/mddj/_cli/read/classifiers.py
@@ -4,8 +4,16 @@ from mddj._cli.state import CommandState, common_args
 
 
 @click.command("classifiers")
+@click.option(
+    "--python-versions",
+    is_flag=True,
+    help=(
+        "Show only the python versions extracted from classifiers, "
+        "limited only to versions of the form MAJOR.MINOR."
+    ),
+)
 @common_args
-def read_classifiers(*, state: CommandState) -> None:
+def read_classifiers(*, state: CommandState, python_versions: bool) -> None:
     """Read the classifiers of the current project."""
-    for c in state.dj.read.classifiers():
+    for c in state.dj.read.classifiers(python_versions=python_versions):
         click.echo(c)
